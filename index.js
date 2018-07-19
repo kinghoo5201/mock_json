38d74f30309e658d502ca10ac27d172015c6542d
kinghoo <kinghoo@kinghoodeMacBook-Air.local>
2018-07-19 11:23:39 +0800

    feat:debug

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/', (req, res, next) => {
     });
 });
 
-app.get('/api', (req, res, next) => {
+app.use('/api', (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");

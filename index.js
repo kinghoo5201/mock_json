f4f3205c613ea6e7820a263275c3de15a00c85af
kinghoo5201 <970489393@qq.com>
2018-07-19 21:57:25 +0800

    feat:open browser

--- index.js
+++ index.js
@@ -1,107 +1,124 @@
-const express = require('express');
-const formidable = require('express-formidable');
-const fs = require('fs');
-const util = require('./util');
-const path = require('path');
+const express = require("express");
+const cp = require("child_process");
+const formidable = require("express-formidable");
+const fs = require("fs");
+const util = require("./util");
+const path = require("path");
 const app = express();
-const jsonDir = './jsons';
-const upDir = './upload';
-const sep = '-||#:#|#:#||-'
+const jsonDir = "./jsons";
+const upDir = "./upload";
+const sep = "-||#:#|#:#||-";
 
 if (!fs.existsSync(jsonDir)) {
-    util.print.red(jsonDir + ' is not exist! start to mkdir' + jsonDir);
-    fs.mkdirSync(jsonDir);
-    util.print.red(jsonDir + ' created!');
+  util.print.red(jsonDir + " is not exist! start to mkdir" + jsonDir);
+  fs.mkdirSync(jsonDir);
+  util.print.red(jsonDir + " created!");
 }
 if (!fs.existsSync(upDir)) {
-    util.print.red(upDir + ' is not exist! start to mkdir' + upDir);
-    fs.mkdirSync(upDir);
-    util.print.red(upDir + ' created!');
+  util.print.red(upDir + " is not exist! start to mkdir" + upDir);
+  fs.mkdirSync(upDir);
+  util.print.red(upDir + " created!");
 }
-app.set('view engine', 'ejs');
-app.set('views', path.resolve(__dirname, 'views'));
-app.set('view cache', false);
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view cache", false);
 
-app.use(formidable({
-    encoding: 'utf-8',
+app.use(
+  formidable({
+    encoding: "utf-8",
     uploadDir: path.resolve(__dirname, upDir),
     keepExtensions: true, //保留后缀
-    multiples: true, //将req.files保存为数组
-
-}));
+    multiples: true //将req.files保存为数组
+  })
+);
 app.use((req, res, next) => {
-    util.print.green(req.method + ' : ' + req.originalUrl);
+  util.print.green(req.method + " : " + req.originalUrl);
+  return next();
+});
+app.get("/", (req, res, next) => {
+  const files = fs.readdirSync(jsonDir);
+  const descs = [];
+  const conts = [];
+  files.forEach(item => {
+    const data = fs.readFileSync(jsonDir + "/" + item, "utf-8");
+    descs.push(data.split(sep)[0]);
+    conts.push(data.split(sep)[1]);
+  });
+  return res.render("index", {
+    files,
+    descs,
+    conts,
+    sep
+  });
+});
+
+app.use("/api", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type,Content-Length, Authorization, Accept,X-Requested-With"
+  );
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", " 3.2.1");
+  if (!req.query.method) {
     return next();
-})
-app.get('/', (req, res, next) => {
-    const files = fs.readdirSync(jsonDir);
-    const descs = [];
-    const conts = [];
-    files.forEach(item => {
-        const data = fs.readFileSync(jsonDir + '/' + item, 'utf-8');
-        descs.push(data.split(sep)[0]);
-        conts.push(data.split(sep)[1]);
-    });
-    return res.render('index', {
-        files,
-        descs,
-        conts,
-        sep
-    });
+  }
+  try {
+    switch (req.query.method) {
+      case "getData":
+        const data = fs.readFileSync(
+          jsonDir + "/" + req.query.data + ".txt",
+          "utf-8"
+        );
+        return res.send(data.split(sep)[1]);
+      default:
+        return next();
+    }
+  } catch (e) {
+    return next();
+  }
 });
 
-app.use('/api', (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By", ' 3.2.1');
-    if (!req.query.method) {
+app.post("/api", (req, res, next) => {
+  if (!req.fields.method) {
+    return next();
+  }
+  try {
+    switch (req.fields.method) {
+      case "addData":
+        const fileName = Date.now() + ".txt";
+        fs.writeFileSync(
+          jsonDir + "/" + fileName,
+          req.fields.desc + sep + req.fields.data,
+          "utf-8"
+        );
+        return res.redirect("back");
+      case "modifyData":
+        const tempName = req.fields.fileName;
+        fs.writeFileSync(
+          jsonDir + "/" + tempName,
+          req.fields.desc + sep + req.fields.data,
+          "utf-8"
+        );
+        return res.redirect("back");
+      case "deleteData":
+        fs.unlinkSync(jsonDir + "/" + req.fields.name);
+        return res.redirect("back");
+      default:
         return next();
     }
-    try {
-        switch (req.query.method) {
-            case 'getData':
-                const data = fs.readFileSync(jsonDir + '/' + req.query.data + '.txt', 'utf-8');
-                return res.send(data.split(sep)[1]);
-            default:
-                return next();
-        }
-    } catch (e) {
-        return next();
-    }
-});
-
-app.post('/api', (req, res, next) => {
-    if (!req.fields.method) {
-        return next();
-    }
-    try {
-        switch (req.fields.method) {
-            case 'addData':
-                const fileName = Date.now() + '.txt';
-                fs.writeFileSync(jsonDir + '/' + fileName, req.fields.desc + sep + req.fields.data, 'utf-8');
-                return res.redirect('back');
-            case 'modifyData':
-                const tempName = req.fields.fileName;
-                fs.writeFileSync(jsonDir + '/' + tempName, req.fields.desc + sep + req.fields.data, 'utf-8');
-                return res.redirect('back');
-            case 'deleteData':
-                fs.unlinkSync(jsonDir + '/' + req.fields.name);
-                return res.redirect('back');
-            default:
-                return next();
-        }
-    } catch (e) {
-        util.print.red('bad post:' + req.originalUrl);
-        return res.redirect('back');
-    }
+  } catch (e) {
+    util.print.red("bad post:" + req.originalUrl);
+    return res.redirect("back");
+  }
 });
 
 app.get((req, res, next) => {
-    util.print.red('bad request::' + req.method + req.originalUrl);
-    return res.send('bad path');
+  util.print.red("bad request::" + req.method + req.originalUrl);
+  return res.send("bad path");
 });
 
 app.listen(2048, () => {
-    util.print.yellow('server is runing at:2048');
+  util.print.yellow("server is runing at:2048\nnow opening browser...");
+  return cp.exec("start http://localhost:2048");
 });

e4f96e54b9fc736730347599737585f73b86755d
kinghoo <kinghoo@kinghoodeMacBook-Air.local>
2018-07-19 11:02:36 +0800

    feat:debut add cors allow

--- index.js
+++ index.js
@@ -45,6 +45,10 @@ app.get('/', (req, res, next) => {
 });
 
 app.get('/api', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1');
     if (!req.query.method) {
         return next();
     }

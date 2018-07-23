90fa7c2dc0b52757ffad28bff12271ebbbc69e72
halifax <970489393@qq.com>
2018-07-23 21:27:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
 });
 app.get("/", (req, res, next) => {
   let files = fs.readdirSync(jsonDir);
-  files.sort();
+  files.reverse();
   const descs = [];
   const conts = [];
   files.forEach(item => {
